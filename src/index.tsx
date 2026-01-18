81eac1699af2c6c488112e0984f17b56c5dbbd67
Jackson Hagood <jagmachat@gmail.com>
2026-01-18 08:43:06 -0600

    Links page

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./style/global.scss";
-import { Terminal } from "./components/terminal/terminal"
+import { Terminal } from "./components/terminal/terminal";
 
-import { layout as about_layout } from "./constants/layouts/about"
-import { layout as resume_layout } from "./constants/layouts/resume"
-import { layout as projects_layout } from "./constants/layouts/projects"
+import { layout as about_layout } from "./constants/layouts/about";
+import { layout as resume_layout } from "./constants/layouts/resume";
+import { layout as projects_layout } from "./constants/layouts/projects";
+import { layout as links_layout } from "./constants/layouts/links";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
@@ -16,8 +17,8 @@ root.render(
       <Terminal
         width = {128}
         height = {32}
-        pages = {["about", "resume", "projects"]}
-        page_layouts = {[about_layout, resume_layout, projects_layout]}
+        pages = {["about", "resume", "projects", "links"]}
+        page_layouts = {[about_layout, resume_layout, projects_layout, links_layout]}
         email = "jagmachat@gmail.com"
       />
     </div>
